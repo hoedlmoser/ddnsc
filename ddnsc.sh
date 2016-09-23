3bba8617c37a4d07b1338fe42852d504587ce366
Klaus M Pfeiffer <kmp.github@kmp.or.at>
2016-09-23 15:35:36 +0200

    initial commit

---
+++ ddnsc.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+scriptdir=$(dirname $0)
+
+. $scriptdir/config
+
+
+log() {
+   logger -t ddnsc -p daemon.notice "$1: $2"
+   echo $(date '+%Y-%m-%d %H:%M:%S') $1: $2
+}
+
+
+update=0
+
+
+if [ -f $scriptdir/lastip ]; then
+  lastIP=$(< $scriptdir/lastip)
+  lastIPepoch=$(date +%s -r $scriptdir/lastip)
+else
+  lastIP="unknown"
+  lastIPepoch=0
+fi
+
+forceEpoch=$(date +%s --date="$force ago")
+
+actIP=$(curl $curlParams $urlIP)
+rc=$?
+
+if [ $rc -ne 0 ]; then
+  log "error" "curl $rc"
+  exit
+fi
+
+actIP=$(echo "$actIP" | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | head -1)
+
+if [ "$lastIP" != "$actIP" ]; then
+  log "info" "ip address change detected ($lastIP --> $actIP)"
+  update=1
+fi
+
+if [ $lastIPepoch -lt $forceEpoch ]; then
+  log "info" "last update $force ago, forcing update"
+  update=1
+fi
+
+if [ $update -eq 1 ]; then
+  response=$(curl $curlParams $urlDdns)
+  rc=$?
+
+  if [ $rc -ne 0 ]; then
+    log "error" "curl $rc"
+    exit
+  fi
+  
+  response=$(echo "$response" | sed 's/<[^>]*>/ /g;s/^\s*//g;s/\s*$//g' | tr -s ' ')
+
+  if [[ "$response" =~ Success ]]; then
+    log "success" "$response"
+    echo -n $actIP > $scriptdir/lastip
+  else
+    log "error" "$response"
+  fi
+
+fi
+
+
